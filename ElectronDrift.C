7017a09f39abda829321d67151c67bb516eaf391
Ella <emajkic@vkdecay.triumf.ca>
2025-02-21 11:20:17 -0800

    New file for sim

---
+++ ElectronDrift.C
@@ -0,0 +1,27 @@
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <regex>
+#include <random>
+
+#include <TApplication.h>
+#include <TCanvas.h>
+#include <TH1F.h>
+
+#include "Garfield/ComponentComsol.hh"
+#include "Garfield/ViewCell.hh"
+#include "Garfield/ViewSignal.hh"
+#include "Garfield/ComponentAnalyticField.hh"
+#include "Garfield/ViewField.hh"
+#include "Garfield/MediumMagboltz.hh"
+#include "Garfield/Sensor.hh"
+#include "Garfield/AvalancheMicroscopic.hh"
+#include "Garfield/AvalancheMC.hh"
+#include "Garfield/ViewDrift.hh"
+#include "Garfield/DriftLineRKF.hh"
+#include "Garfield/Random.hh"
+#include "Garfield/ComponentGrid.hh"
+#include "Garfield/ViewField.hh"
+#include "Garfield/ViewFEMesh.hh"
+#include "Garfield/ViewMedium.hh"
